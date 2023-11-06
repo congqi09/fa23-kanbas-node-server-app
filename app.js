5553ec5aa07c2705dab7b0e2b77bccca93487266
Cong Qi <qi.co@northeastern.edu>
2023-11-05 22:21:07 -0800

    A5 - 2.7

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = require('express')
+import express from 'express';
 const app = express()
 app.get('/hello', (req, res) => {res.send('Life is good!')})
 app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
