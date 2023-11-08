a64ad61cc79474a69339d128f5c7d5e831281467
Cong Qi <qi.co@northeastern.edu>
2023-11-07 16:44:30 -0800

    2.8

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from 'express';
+import Hello from './hello.js';
 const app = express()
-app.get('/hello', (req, res) => {res.send('Life is good!')})
-app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
-app.listen(4000)
+// app.get('/hello', (req, res) => {res.send('Life is good!')})
+// app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
 // qq: could nodemon trigger the front-end auto-refresh?
+Hello(app)
+app.listen(4000)
